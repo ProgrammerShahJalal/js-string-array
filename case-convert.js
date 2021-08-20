2f5ebbe11b5dfa09aab1abcc10272ea4c42e9f0d
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-08-20 22:03:47 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,11 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobasi';
+const search = 'valo';
+
+const userInput = 'blackPinK';
+const savedUserName = 'blackPink';
+if (userInput.toLowerCase == savedUserName.toLowerCase) {
+    console.log('user exists');
+}
+else {
+    console.log('user not exists');
+}
